58194bdf612e717c9a3101f9cfef7e505d1916bc
Austin Dudas <dudasaus@umich.edu>
2024-08-05 22:07:10 -0600

    Server mocking testing examples

--- frontend/src/components/backend_checker/BackendChecker.test.tsx
+++ frontend/src/components/backend_checker/BackendChecker.test.tsx
@@ -1,12 +1,46 @@
-import { describe, it } from 'vitest';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest';
+import { render, fireEvent, screen, cleanup } from '@testing-library/react';
 import { BackendChecker } from './BackendChecker';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 
 describe('BackendChecker', () => {
-  it('checks', async () => {
+  const server = setupServer(
+    http.get(`${import.meta.env.VITE_BACKEND_URL}/pulse`, () => {
+      return HttpResponse.json({ msg: 'ok' });
+    }),
+  );
+
+  beforeAll(() => server.listen());
+  afterEach(() => {
+    server.resetHandlers();
+    cleanup();
+  });
+  afterAll(() => server.close());
+
+  it('is ok when the backend responds', async () => {
+    render(<BackendChecker />);
+
+    await fireEvent.click(screen.getByText('Check backend connection'));
+    await screen.findByText('ok');
+  });
+
+  it('shows an error when the backend fails', async () => {
+    server.use(
+      http.get(`${import.meta.env.VITE_BACKEND_URL}/pulse`, () => {
+        return HttpResponse.error();
+      }),
+    );
+
     render(<BackendChecker />);
 
     await fireEvent.click(screen.getByText('Check backend connection'));
     await screen.findByText('error');
+  });
+
+  it(`cleans up the dom in between tests`, async () => {
+    expect(async () => {
+      await screen.findByText('Check backend connection');
+    }).rejects.toThrowError();
   });
 });
