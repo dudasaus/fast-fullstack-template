75c042ac205d0d773f73fe8fd0a917113612193e
Austin Dudas <dudasaus@umich.edu>
2024-08-05 18:19:05 -0600

    Prettier formatting

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.get("/pulse", (_req, res) => {
+app.get('/pulse', (_req, res) => {
   res.status(200).json({
-    msg: "ok",
+    msg: 'ok',
   });
 });
 
